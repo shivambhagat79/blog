7212cc507c487bf7d137bdaf9761b9fbec5a11aa
shivambhagat79 <shivambhagat2279+github@gmail.com>
2023-07-19 00:51:11 +0530

    initial commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,33 @@
+<div class="navbar bg-base-200 fixed">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul  class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52" >
+        <li><a>Home</a></li>
+        <li><a>My Life</a></li>
+        <li><a>My Achievements</a></li>
+        <li><a>My Skills</a></li>
+        
+      </ul>
+    </div>
+    <a href="/" class="btn btn-ghost normal-case text-xl">Shivam Bhagat</a>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1 text-lg">
+      <li><a>Home</a></li>
+      <li><a>My Life</a></li>
+      <li><a>My Achievements</a></li>
+      <li><a>My Skills</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+   
+  </div>
+</div>
+
+
+<style>
+
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+    import Navbar from "../components/Navbar.svelte";
+    import Footer from "../components/Footer.svelte";
+    export const prerender = true;
+</script>
+
+<Navbar />
+
+<slot />
+
+<Footer />
