b29dd137ca6b3b848c33ccee0c44aa3971f0bcae
shivambhagat79 <shivambhagat2279+github@gmail.com>
2023-07-22 12:55:55 +0530

    commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import "../app.css";
     import Navbar from "../components/Navbar.svelte";
     import Footer from "../components/Footer.svelte";
-    export const prerender = true;
+    
 </script>
 
 <Navbar />
--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/blog' : ''
+			base: ''
 		}
 	}
 };
